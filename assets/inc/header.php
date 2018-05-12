915dcfa049535eacc2582b46ae62657883bf2493
R-P-Shah <30637349+R-P-Shah@users.noreply.github.com>
2018-05-12 12:26:11 -0400

    Add files via upload

---
+++ assets/inc/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+
+<html>
+
+<head>
+    <title>Rahul Shah's Website</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css" />
+
+    
+</head>
+<body>
+<div class="banner">
+    <img id = "header_img" src="/assets/images/homepage_header.jpg">
+</div>
